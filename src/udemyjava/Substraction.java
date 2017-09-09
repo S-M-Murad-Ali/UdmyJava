43e9f2fd5c248d18fe27bebf89631cb521409c21
smmur <smmur@DESKTOP-EQ49A5K.Home>
2017-09-09 12:22:58 +0100

    Subtraction

---
+++ src/udemyjava/Substraction.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udemyjava;
+
+/**
+ *
+ * @author smmur
+ */
+public class Substraction {
+    public static void main (String args[]){
+        
+        int i=10;
+        int j=20;
+        int k=i+j;
+        System.out.println(k);
+        
+        
+    }
+}
